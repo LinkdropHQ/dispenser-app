fdec65c42d4b42d2e0484984e4751759408eb440
spacehaz <spacehaz@gmail.com>
2024-05-15 13:10:46 +0300

    update dev dispenser url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const dashboardServerUrl = 'https://dashboard-api.linkdrop.io'
-export const devDashboardServerUrl = 'https://dev.dashboard-api.linkdrop.io'
+export const devDashboardServerUrl = 'https://linkdrop-api-main-4c33f49.d2.zuplo.dev'
 export const testnetsDashboardServerUrl = 'https://testnets.dashboard-api.linkdrop.io'
