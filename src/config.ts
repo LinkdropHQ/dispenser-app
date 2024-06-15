44042af646a1dc59bd19fb4e8a13b3fa441dd085
Mikhail <m.dobrokhvalov@gmail.com>
2024-06-15 07:04:39 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -16,5 +16,6 @@ export const customClaimApps: TCustomClaimApps = {
 export const customClaimAppsForToken: TCustomClaimApps = {
   "0x849151d7d0bf1f34b70d5cad5149d28cc2308bf1": "https://iloveyou.linkdrop.io",
   "0xfc446a839690158933a039477fff44ec45628f01": "https://iloveyou.linkdrop.io",
-   "0xb019a71e796db085bbf7fadb5a95db7a2ae2a3c2": "https://iloveyou.linkdrop.io"
+   "0xb019a71e796db085bbf7fadb5a95db7a2ae2a3c2": "https://iloveyou.linkdrop.io", 
+   "0x59874722007b259413ea90bbe61ea970d9dbe464": "https://iloveyou.linkdrop.io"
 }
