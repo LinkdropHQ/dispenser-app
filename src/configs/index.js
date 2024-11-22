189071d625160184be04d4c19b8ca3c4ddccaf12
spacehaz <spacehaz@gmail.com>
2024-11-22 11:39:13 +0300

    added zuplo key for prod

--- src/configs/index.js
+++ src/configs/index.js
@@ -3,4 +3,4 @@ export const devDashboardServerUrl = 'https://linkdrop-api-staging-715c814.zuplo
 export const testnetsDashboardServerUrl = 'https://testnets.dashboard-api.linkdrop.io'
 
 export const devApiKey = 'zpka_1365718c556e4f28904657c528730556_3f9a6532'
-export const apiKey = 'zpka_98b96ff1674e4302a5f6d1ada8c627b8_13770bbc'
+export const apiKey = 'zpka_907ffc120f0e4491a75a6c55aa93a65a_4b20a148'
