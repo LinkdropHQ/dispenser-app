57e800a5d0558bc3ee500d5772bb069c16fc4b6f
Hazo Baikulov <spacehaz@gmail.com>
2025-07-01 17:29:08 +0300

    updates for redirect link check

--- src/scripts/helpers/index.ts
+++ src/scripts/helpers/index.ts
@@ -1,9 +1,11 @@
 import checkIfMultiscanIsPresented from './check-if-multiscan-is-presented'
 import defineApiURL from './define-api-url'
 import defineApiHeaders from './define-api-headers'
+import transferQueryParams from './transfer-query-params'
 
 export {
   checkIfMultiscanIsPresented,
   defineApiURL,
-  defineApiHeaders
+  defineApiHeaders,
+  transferQueryParams
 }
